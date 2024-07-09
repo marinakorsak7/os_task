bc440ad0e7f9f28770bdc924fda9b5d3daa365d4
marinakorsak7 <73583097+marinakorsak7@users.noreply.github.com>
2024-07-09 23:22:43 +0200

    server and client update

--- client.c
+++ client.c
@@ -1,59 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include "kv_lib.h"
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
 
-#define SERVER_PIPE "/tmp/server.fifo"
+#define SERVER_SOCKET_PATH "/tmp/server_socket"
+#define CLIENT_SOCKET_TEMPLATE "/tmp/client_socket_%d"
 #define MAX_BUF_SIZE 256
 
 void print_usage(char *program_name) {
     fprintf(stderr, "Usage: %s <command>\n", program_name);
 }
 
-int create_client_fifo(char *client_fifo) {
-    if (establish_connection(SERVER_PIPE, client_fifo) == -1) {
+int create_client_socket(char *client_socket_path) {
+    snprintf(client_socket_path, MAX_BUF_SIZE, CLIENT_SOCKET_TEMPLATE, getpid());
+    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
         return -1;
     }
+    
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, client_socket_path, sizeof(addr.sun_path) - 1);
+
+    unlink(client_socket_path);
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+        perror("bind");
+        close(sockfd);
+        return -1;
+    }
+
+    printf("Created client socket: %s\n", client_socket_path);
+    return sockfd;
+}
+
+int connect_to_server(int sockfd) {
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SERVER_SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+        perror("connect");
+        return -1;
+    }
+
+    printf("Connected to server socket: %s\n", SERVER_SOCKET_PATH);
     return 0;
 }
 
-int open_fifos(const char *client_fifo, int *client_fd, int *server_fd) {
-    *client_fd = open(client_fifo, O_RDONLY);
-    *server_fd = open(SERVER_PIPE, O_WRONLY);
-
-    if (*server_fd == -1 || *client_fd == -1) {
-        perror("open");
-        if (*client_fd != -1) close(*client_fd);
+int transmit_command(int sockfd, const char *command) {
+    if (write(sockfd, command, strlen(command) + 1) == -1) {
+        perror("write");
         return -1;
     }
+    printf("Transmitted command: %s\n", command);
     return 0;
 }
 
-int transmit_command(int server_fd, const char *command) {
-    if (send_data(server_fd, command) == -1) {
-        perror("send_data");
-        return -1;
-    }
-    return 0;
-}
-
-void receive_response(int client_fd) {
+void receive_response(int sockfd) {
     char response[MAX_BUF_SIZE];
-    int bytes_received = receive_data(client_fd, response, MAX_BUF_SIZE);
+    int bytes_received;
 
+    bytes_received = read(sockfd, response, MAX_BUF_SIZE);
     if (bytes_received > 0) {
         response[bytes_received] = '\0';
         printf("Server response: %s\n", response);
+    } else if (bytes_received == -1) {
+        perror("read");
     }
 }
 
-void cleanup(int client_fd, const char *client_fifo) {
-    terminate_connection(client_fd);
-    unlink(client_fifo);
+void cleanup(int sockfd, const char *client_socket_path) {
+    close(sockfd);
+    unlink(client_socket_path);
+    printf("Closed and removed client socket: %s\n", client_socket_path);
 }
 
 int main(int argc, char *argv[]) {
@@ -62,25 +88,27 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    char client_fifo[MAX_BUF_SIZE];
-    int client_fd, server_fd;
+    char client_socket_path[MAX_BUF_SIZE];
+    int client_sockfd;
 
-    if (create_client_fifo(client_fifo) == -1) {
+    client_sockfd = create_client_socket(client_socket_path);
+    if (client_sockfd == -1) {
         exit(EXIT_FAILURE);
     }
 
-    if (open_fifos(client_fifo, &client_fd, &server_fd) == -1) {
-        unlink(client_fifo);
+    if (connect_to_server(client_sockfd) == -1) {
+        cleanup(client_sockfd, client_socket_path);
         exit(EXIT_FAILURE);
     }
 
-    if (transmit_command(server_fd, argv[1]) == -1) {
-        cleanup(client_fd, client_fifo);
+    if (transmit_command(client_sockfd, argv[1]) == -1) {
+        cleanup(client_sockfd, client_socket_path);
         exit(EXIT_FAILURE);
     }
 
-    receive_response(client_fd);
-    cleanup(client_fd, client_fifo);
+    receive_response(client_sockfd);
+    cleanup(client_sockfd, client_socket_path);
 
     return 0;
 }
+
--- server.c
+++ server.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <signal.h>
 #include <errno.h>
 #include "kv_lib.h"
 
-#define SERVER_PIPE "/tmp/server.fifo"
+#define SERVER_SOCKET_PATH "/tmp/server_socket"
 #define MAX_BUFFER_SIZE 256
 
 typedef struct {
@@ -24,7 +24,7 @@ volatile sig_atomic_t server_active = 1;
 void signal_handler(int sig) {
     (void)sig;
     server_active = 0;
-    unlink(SERVER_PIPE);
+    unlink(SERVER_SOCKET_PATH);
     exit(0);
 }
 
@@ -37,84 +37,102 @@ int search_key(const char* key) {
     return -1;
 }
 
-void process_client_request(const char* client_fifo) {
-    int client_fd;
-    while ((client_fd = open(client_fifo, O_WRONLY)) == -1) {
-        if (errno != ENOENT) {
-            perror("open client_fifo");
+void process_client_request(int client_sockfd) {
+    char buffer[MAX_BUFFER_SIZE];
+    int bytes_received = read(client_sockfd, buffer, MAX_BUFFER_SIZE);
+
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        printf("Processing request: %s\n", buffer);
+
+        char* command = strtok(buffer, " ");
+        if (command == NULL) {
             return;
         }
-        usleep(10000);
-    }
 
-    char buffer[MAX_BUFFER_SIZE];
-    int bytes_read = receive_data(client_fd, buffer, MAX_BUFFER_SIZE);
-    if (bytes_read > 0) {
-        buffer[bytes_read] = '\0';
-        char* command = strtok(buffer, " ");
         if (strcmp(command, "set") == 0) {
             char* key = strtok(NULL, " ");
             char* value = strtok(NULL, "\n");
-            int index = search_key(key);
-            if (index == -1) {
-                strcpy(kv_store[kv_count].key, key);
-                strcpy(kv_store[kv_count].value, value);
-                kv_count++;
-            } else {
-                strcpy(kv_store[index].value, value);
+            if (key != NULL && value != NULL) {
+                int index = search_key(key);
+                if (index == -1) {
+                    strcpy(kv_store[kv_count].key, key);
+                    strcpy(kv_store[kv_count].value, value);
+                    kv_count++;
+                } else {
+                    strcpy(kv_store[index].value, value);
+                }
+                printf("Set key: %s, value: %s\n", key, value);
+                write(client_sockfd, "OK", 3);
             }
         } else if (strcmp(command, "get") == 0) {
             char* key = strtok(NULL, "\n");
-            int index = search_key(key);
-            char response[MAX_BUFFER_SIZE];
-            if (index == -1) {
-                snprintf(response, sizeof(response), "Key not found");
-            } else {
-                snprintf(response, sizeof(response), "Value: %s", kv_store[index].value);
+            if (key != NULL) {
+                int index = search_key(key);
+                char response[MAX_BUFFER_SIZE];
+                if (index == -1) {
+                    snprintf(response, sizeof(response), "Key not found");
+                } else {
+                    snprintf(response, sizeof(response), "Value: %s", kv_store[index].value);
+                }
+                write(client_sockfd, response, strlen(response) + 1);
+                printf("Get key: %s, response: %s\n", key, response);
             }
-            send_data(client_fd, response);
         }
     }
-    close(client_fd);
+
+    close(client_sockfd);
 }
 
 void cleanup() {
-    unlink(SERVER_PIPE);
+    unlink(SERVER_SOCKET_PATH);
 }
 
 int main() {
     signal(SIGTERM, signal_handler);
 
-    if (access(SERVER_PIPE, F_OK) == 0) {
-        unlink(SERVER_PIPE);
+    int server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_sockfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
     }
 
-    if (mkfifo(SERVER_PIPE, 0666) == -1) {
-        perror("mkfifo");
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SERVER_SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    unlink(SERVER_SOCKET_PATH);
+    if (bind(server_sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+        perror("bind");
+        close(server_sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_sockfd, 5) == -1) {
+        perror("listen");
+        close(server_sockfd);
         exit(EXIT_FAILURE);
     }
 
     printf("Server is running...\n");
 
-    int server_fd = open(SERVER_PIPE, O_RDONLY);
-    if (server_fd == -1) {
-        perror("open server_fifo");
-        cleanup();
-        exit(EXIT_FAILURE);
-    }
-
     while (server_active) {
-        char buffer[MAX_BUFFER_SIZE];
-        int bytes_read = receive_data(server_fd, buffer, MAX_BUFFER_SIZE);
-        if (bytes_read > 0) {
-            buffer[bytes_read] = '\0';
-            printf("Received request: %s\n", buffer);
-            char* client_fifo = buffer + 8;
-            process_client_request(client_fifo);
+        int client_sockfd = accept(server_sockfd, NULL, NULL);
+        if (client_sockfd == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                perror("accept");
+                break;
+            }
         }
+
+        process_client_request(client_sockfd);
     }
 
-    close(server_fd);
+    close(server_sockfd);
     cleanup();
     return 0;
 }
+
